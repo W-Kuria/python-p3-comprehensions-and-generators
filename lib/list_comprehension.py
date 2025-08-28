d78f49d09ea4a68afa09a571e7e61767f261b88b
W-Kuria <william.njoroge.kuria@gmail.com>
2025-08-28 08:56:24 +0300

    feat: complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def return_evens(sequence):
+    """
+    Takes a sequence of integers and returns only the even ones.
+    Example: return_evens([0, 1, 3, 5, 7, 8, 9]) -> [0, 8]
+    """
+    return [n for n in sequence if n % 2 == 0]
+
+
+def make_exclamation(sentences):
+    """
+    Takes a list of sentences and returns them with '!' at the end.
+    Example: make_exclamation(["Hello", "Python is fun"]) -> ["Hello!", "Python is fun!"]
+    """
+    return [sentence + "!" for sentence in sentences]
